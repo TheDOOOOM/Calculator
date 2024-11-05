3dbcef40aefa4525f35ea17a939f99d37940e050
TheDOOOOM <kuricindaniil442@gmail.com>
2024-11-05 17:17:48 +0500

    LoadSceeneComplited

--- Assets/Calculator/_Scripts/LoadingScreen/LoadingScreen.cs
+++ Assets/Calculator/_Scripts/LoadingScreen/LoadingScreen.cs
@@ -1,8 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadingScreen : MonoBehaviour
 {
-    
+    [SerializeField] private Slider _slider;
+
+    private void Start()
+    {
+        LoadNextScreen();
+    }
+
+    private async void LoadNextScreen()
+    {
+        var loadProgrees = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        while (!loadProgrees.isDone)
+        {
+            await UniTask.Yield(PlayerLoopTiming.Update);
+        }
+    }
 }
